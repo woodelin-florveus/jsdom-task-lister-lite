52d24ee3c44fa6f85ccba9dc97b1fc8c8bc1721b
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-03 21:16:11 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  
+  
+  
+  const createTasks = document.getElementById('create-task-form')
+  
+  
+  createTasks.addEventListener('submit', formTask)
+  
+    function formTask(event){
+
+       event.preventDefault()
+
+    const newTask = document.querySelector('#new-task-description').value
+
+    const li = document.createElement('li')
+
+    li.textContent = newTask
+
+    const parentTasks = document.getElementById('tasks')
+
+    parentTasks.append(li)
+
+    }
+
 });
+
+
+
